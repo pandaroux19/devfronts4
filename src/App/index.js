080c2e75935dbbca8cd10cd77d4ae493c198f76f
Killian <killianm2259@gmail.com>
2024-01-15 14:16:51 +0100

    3. emeraude

---
+++ src/App/index.js
@@ -0,0 +1,29 @@
+import {t} from '../Data/data.js';
+import '../App.css';
+import styles from './App.module.css';
+import Task from '../Task/index.js'
+
+function App() {
+  let toto="Hello Toto ! !"
+  document.querySelectorAll("li").forEach(element => {
+    element.addEventListener("click",()=>{
+      console.log(element.innerText)
+    })
+  });
+  return (
+    <div className="App">
+      Hello !!
+      <h1>{toto}</h1>
+      <ul>
+        {t.map(p => <li key={p.id}>{p.name}</li>)}
+        <Task/>
+      </ul>
+      <h2 className={styles.title}>Test</h2>
+    </div>
+  );
+}
+
+
+
+
+export default App;
---
+++ src/Task/index.js
@@ -0,0 +1,7 @@
+import React, {ReactElement} from 'react';
+
+export default function Task() {
+    return (
+        <li>Nouvelle tâche</li>
+    );
+}
