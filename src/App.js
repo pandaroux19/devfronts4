05ad18ff39126e31a93bdcf95f2acc158b16215a
duez <benoitduez1909@gmail.com>
2024-01-15 11:58:42 +0100

    exo1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
+import {t} from './Data/data.js';
 import './App.css';
 
 function App() {
   let toto="Hello Toto ! !"
+  let tableau = t.map(p => <li key={p.id}>{p.name}</li>)
+ 
   return (
     <div className="App">
       Hello !!
       <h1>{toto}</h1>
+
+      <ul>
+{tableau}
+      </ul>
+
     </div>
+
+    
   );
 }
+
+
+
 
 export default App;
---
+++ src/Data/data.js
@@ -0,0 +1,5 @@
+let t = [{id:"0",name:"toto"},
+        {id:"1",name: "marie"},
+        {id:"2",name: "jean"}]
+
+export {t}
