22c61e9206971793febec381ff41a6f610404424
Killian <killianm2259@gmail.com>
2024-01-15 15:18:36 +0100

    3. emeraude

--- src/App/index.js
+++ src/App/index.js
@@ -10,20 +10,12 @@ function App() {
   //     console.log(element.innerText)
   //   })
   // });
-  function click(e){
-    console.log(e.name);
-  }
-  function barrler(){
-    this.classList.contains(styles.barrer) ? this.classList.remove(styles.barrer) : this.classList.add(styles.barrer)
-
-  }
   return (
     <div className="App">
       Hello !!
       <h1>{toto}</h1>
       <ul>
-        {t.map(p => <li key={p.id} onDoubleClick={()=>click(p)} onClick={()=>barrler()}>{p.name}</li>)}
-        <Task/>
+        {t.map(p => <Task {...p}/>)}
       </ul>
       <h2 className={styles.title}>Test</h2>
     </div>
--- src/Task/index.js
+++ src/Task/index.js
@@ -1,7 +1,16 @@
 import React, {ReactElement} from 'react';
 
-export default function Task() {
+export default function Task(props) {
+    function click(e){
+        console.log(e.name);
+      }
+      function barrler(){
+        this.classList.contains(styles.barrer) ? this.classList.remove(styles.barrer) : this.classList.add(styles.barrer)
+    
+      }
     return (
-        <li>Nouvelle tâche</li>
+        <div>
+            <li key={props.id} onDoubleClick={()=>click(props)} onClick={()=>barrler()}>{props.name}</li>
+        </div>
     );
 }
