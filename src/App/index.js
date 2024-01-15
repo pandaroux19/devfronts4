ba658e1f3a845b6c68c9368337bc3c531fae60e2
duez <benoitduez1909@gmail.com>
2024-01-15 14:59:41 +0100

    si tu fait mon travail je te tue :)

--- src/App/index.js
+++ src/App/index.js
@@ -13,12 +13,16 @@ function App() {
   function click(e){
     console.log(e.name);
   }
+  function barrler(){
+    this.classList.contains(styles.barrer) ? this.classList.remove(styles.barrer) : this.classList.add(styles.barrer)
+
+  }
   return (
     <div className="App">
       Hello !!
       <h1>{toto}</h1>
       <ul>
-        {t.map(p => <li key={p.id} onDoubleClick={()=>click(p)}>{p.name}</li>)}
+        {t.map(p => <li key={p.id} onDoubleClick={()=>click(p)} onClick={()=>barrler()}>{p.name}</li>)}
         <Task/>
       </ul>
       <h2 className={styles.title}>Test</h2>
